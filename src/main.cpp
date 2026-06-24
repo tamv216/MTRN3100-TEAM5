5eceb6ed11b65a65a70e80ade9a8db15b39a1b85
Giselle <gisellelevinewijaya9@gmail.com>
2026-06-24 13:28:30 +1000

    feat(lab):put test code into main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
-#include <Arduino.h>
+#include "Encoder.hpp"
+#include "Motor.hpp"
+#include "PIDController.hpp"
+
+#define MOT1PWM 9 // PIN 9 is a PWM pin
+#define MOT1DIR 10
+mtrn3100::Motor motor(MOT1PWM,MOT1DIR);
+
+#define EN_A 2 // PIN 2 is an interupt
+#define EN_B 4
+mtrn3100::Encoder encoder(EN_A, EN_B);
+
+mtrn3100::PIDController new_controller(5000,0,1000);
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  new_controller.zeroAndSetTarget(encoder.getRotationRadians(), PI);
+  
+  encoder.resetCount();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float angle = encoder.getRotationRadians();
+
+  float u = new_controller.compute(angle);
+
+  motor.setPWM((int16_t)u);
+
+  Serial.println(angle);
+
+  delay(10);
 }
