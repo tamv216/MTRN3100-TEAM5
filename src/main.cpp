ccfad1010e9468a82bab3af148681a49c81d2824
Nipun Lokuwithanage <nipun.lok@gmail.com>
2026-06-25 14:11:43 +1000

    feat(task1): testing PID

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,88 @@
 #include <Motor.hpp>
 #include <PIDController.hpp>
 
-#define MOT1PWM 9 // PIN 9 is a PWM pin
-#define MOT1DIR 10
-mtrn3100::Motor motor(MOT1PWM,MOT1DIR);
+#define WHEEL_D 32.0
+#define WHEELBASE 96.0
 
-#define EN_A 2 // PIN 2 is an interupt
-#define EN_B 4
-mtrn3100::Encoder encoder(EN_A, EN_B);
+#define MOT1PWM 11 // PIN 11 is a PWM pin
+#define MOT1DIR 12
 
-mtrn3100::PIDController new_controller(5000,0,1000);
+#define MOT2PWM 9 // PIN 9 is a PWM pin
+#define MOT2DIR 10
 
+
+#define EN_A1 2 // PIN 2 is an interupt
+#define EN_B1 7
+
+#define EN_A2 3 // PIN 3 is an interupt
+#define EN_B2 8
+
+typedef enum {
+  Left,
+  Right
+} MotorSide;
+
+mtrn3100::Motor motorL(MOT1PWM,MOT1DIR);
+mtrn3100::Motor motorR(MOT2PWM,MOT2DIR);
+
+mtrn3100::Encoder<EN_A1, EN_B1> encoderL;
+mtrn3100::Encoder<EN_A2, EN_B2> encoderR;
+
+mtrn3100::PIDController controllerL(5000,0,0);
+mtrn3100::PIDController controllerR(5000,0,0);
+
+uint64_t time;
+
+/**
+ * Converts robot translation to equivalent wheel rotation
+ */
+static inline float distToRadians(float dist) {
+  return dist / (PI * WHEEL_D);
+}
+
+/**
+ * Converts heading change to equivalent wheel rotation 
+ */
+static inline float headingToDistance(float angleRad) {
+  return angleRad * WHEELBASE / 2;
+}
+
+static inline float positionPath(uint64_t time, MotorSide motor) {
+  float target = 2000;
+
+  float rot = headingToDistance(PI/4);
+  rot *= (motor == Right) ? -1 : 1;
+
+  int n = (((time - 2000) / 500) % 4) + 1;
+  n *= (time >= 2000 && time < 6000) ? 1 : -1;
+  return target + n * rot;
+}
 
 void setup() {
   Serial.begin(9600);
-  new_controller.zeroAndSetTarget(encoder.getRotationRadians(), PI);
-  
-  encoder.resetCount();
+  encoderL.resetCount();
+  encoderR.resetCount();
+  delay(2000);
 }
 
 void loop() {
-  float angle = encoder.getRotationRadians();
+  // motorL.setPWM((int16_t)255);
+  // float pL =  positionPath(time, Left);
+  // float pR =  positionPath(time, Right);
+  controllerL.zeroAndSetTarget(encoderL.getRotationRadians(), PI);//pL);
+  // controllerR.zeroAndSetTarget(encoderR.getRotationRadians(), pR);
+  float angleL = encoderL.getRotationRadians();
+  // float angleR = encoderR.getRotationRadians();
 
-  float u = new_controller.compute(angle);
+  float uL = controllerL.compute(angleL);
+  // float uR = controllerR.compute(angleR);
+  Serial.println(uL);
 
-  motor.setPWM((int16_t)u);
+  motorL.setPWM((int16_t)uL);
+  // motorR.setPWM((int16_t)uR);
 
-  Serial.println(angle);
-
-  delay(10);
+  Serial.println(angleL);
+  // Serial.println(angleR);
+  delay(1);
+  // time = millis();
 }
